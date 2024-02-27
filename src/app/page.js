efdc45abbdcb7fb4a5a9391c2a1d08ea07a6c520
DANIEL WILSON <daniel.j.wilson@btinternet.com>
2024-02-27 16:28:49 +0000

    database-dodgystyling

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,19 @@
 import Image from "next/image";
+import { sql } from "@vercel/postgres";
 
-export default function Home() {
+export default async function Posts() {
+  const posts = await sql`SELECT * FROM posts;`;
+
   return (
-    <div>
-      <h1>Hello</h1>
+    <div className="m-6 border-2 border-solid border-red-300">
+      <h1 className="text-center text-4xl font-bold">POSTS</h1>
+      <ul>
+        {posts.rows.map((post) => (
+          <li key={post.id} className="text-center text-indigo-500 m-2">
+            {post.title}:{post.content}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
