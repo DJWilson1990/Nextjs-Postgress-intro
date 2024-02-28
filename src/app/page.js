16d4d56f434e10ff79830277f7d9b370ff53fb32
DANIEL WILSON <daniel.j.wilson@btinternet.com>
2024-02-28 09:17:52 +0000

    testing database

--- src/app/page.js
+++ src/app/page.js
@@ -4,9 +4,13 @@ import { sql } from "@vercel/postgres";
 export default async function Posts() {
   const posts = await sql`SELECT * FROM posts;`;
 
+  //   const [post] = await sql`
+  //   SELECT * FROM posts WHERE id = ${params.id};
+  // `;
+
   return (
-    <div className="m-6 border-2 border-solid border-red-300">
-      <h1 className="text-center text-4xl font-bold">POSTS</h1>
+    <div className="m-6 border-2 border-solid border-red-300 bg-gray-100">
+      <h1 className="text-center text-4xl font-bold m-4">POSTS</h1>
       <ul>
         {posts.rows.map((post) => (
           <li key={post.id} className="text-center text-indigo-500 m-2">
